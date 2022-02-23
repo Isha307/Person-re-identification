9e492818188a21982205a84cec0e209db3d335bc
Naresh <56593631+naresh2002@users.noreply.github.com>
2022-02-23 16:28:01 +0530

    Update __init__.py

--- reid/evaluation_metrics/__init__.py
+++ reid/evaluation_metrics/__init__.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
 from .classification import accuracy
-from .ranking import map_cmc
+from .ranking import cmc, mean_ap, map_cmc
 
 __all__ = [
     'accuracy',
+    'cmc',
+    'mean_ap',
     'map_cmc',
 ]
